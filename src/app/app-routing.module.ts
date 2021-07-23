1326bd813001f33a30db56264c1a15aaf168dd40
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-07-23 19:11:48 +0200

    init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { DetailComponent } from './detail/detail.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: BlogComponent },
+  { path: 'portfolio/detail', component: DetailComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
