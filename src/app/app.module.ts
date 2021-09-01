adee4879ef0724811f76bd1ec593fddc8b881345
Yosr Abdelhamid <yosr.abdelhamid97@gmail.com>
2021-09-01 15:11:17 +0100

    Projet Finale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,14 +11,16 @@ import { ContactComponent } from './contact/contact.component';
 import { DetailComponent } from './detail/detail.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { AddDevisComponent } from './add-devis/add-devis.component';
+import { ListeComponent } from './liste/liste.component';
+import { SendmailComponent } from './sendmail/sendmail.component';
 
 @NgModule({
   declarations: [
@@ -35,13 +36,18 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     RegisterComponent,
     BoardAdminComponent,
     BoardModeratorComponent,
-    BoardUserComponent
+    BoardUserComponent,
+    AddDevisComponent,
+    ListeComponent,
+    SendmailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+   
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Chantier } from '../models/chantier.model';
+import { ChantierService } from '../_services/chantier.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  id?: number;
+  chantier?: Chantier;
 
-  constructor() { }
+  chantiers?: Chantier[];
+  
+  constructor(private route: ActivatedRoute,private router: Router,
+    private chantierService: ChantierService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.reloadData();
+
+    this.chantier = new Chantier();
+
+    this.id = this.route.snapshot.params['id'];
+    
+    this.chantierService.getChantier(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.chantier = data;
+      }, error => console.log(error));
+
+      
+  }
+  reloadData(): void  {
+    this.chantierService.getListFiles()
+      .subscribe(
+        data => {
+          this.chantiers = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
+  list(){
+    this.router.navigate(['blog']);
+  }
 }
