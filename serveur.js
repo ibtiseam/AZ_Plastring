db4b990e882da95eb6773508ee36ba80917b75d0
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-09-02 19:59:25 +0200

    paramétrage pour déploiement de l'application heroku

---
+++ serveur.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const app = express();
+
+app.use(express.static(__dirname+ './dist/projet'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname+ './dist/projet/index.html'));
+  });
+
+  app.listen(process.env.PORT);
