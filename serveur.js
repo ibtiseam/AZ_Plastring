834d7076c2565962b7181f2b5226e5a2e3b3f2c4
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-09-02 21:50:46 +0200

    paramétrage pour déploiement de l'application heroku

--- serveur.js
+++ serveur.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname+ './dist/projet'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/projet'));
 
-app.get(`/*`, function(req, res) {
-  res.sendFile(`index.html`, {root: `dist/projet/`}
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/projet/'}),
 );
-});
 
-  app.listen(process.env.PORT);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
